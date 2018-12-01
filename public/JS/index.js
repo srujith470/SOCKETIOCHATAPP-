1f5c055a6e2d9e983eafd9a04bb31f61530addc3
srujith470 <srujith.reddy2012@gmail.com>
2018-12-01 11:34:55 -0500

    listner and emitter

--- public/JS/index.js
+++ public/JS/index.js
@@ -2,28 +2,20 @@ var socket =  io(); //we are making a connection from client to server and keep 
     socket.on('connect', function()  {
         console.log('log statement in index.html');
 
-        socket.emit('recieveEmail', {
-            to:'    asdfghjkl@gmail.com',
-            text:'HAI HOW ARE YOU',
-            createdAt:12345678
-        }); //EMITTER
+        // socket.emit('createMessage', {
+        //     from:'aamir',
+        //     text:'EMIT MESSAGE',
+        //     createdAt:0987654321
+        // }); //EMITTER
 
-        socket.emit('createMessage', {
-            from:'aamir',
-            text:'EMIT MESSAGE',
-            createdAt:0987654321
-        }); //EMITTER
+    });
 
+    socket.on('createMessage', function(message) {
+        console.log(message);
     });
 
     socket.on('disconnect', function()  {
         console.log('disconnect server index.html');
     });
 
-    socket.on('newEmail', function(email) {
-        console.log('New Email', email); // LISTNER EMAIL RECIEVED
-    });
 
-    socket.on('newMessage', function(message) {
-        console.log('newMessage', message); // LISTNER MESSAGE RECIEVED
-    });
--- server/server.js
+++ server/server.js
@@ -1,16 +1,58 @@
-const path = require("path");
+const path = require("path"); // builtin module
 const express = require("express");
+const socketIO = require('socket.io');
+const http = require('http') // built in module
 
 const port = process.env.PORT  || 3000
 const publicPath = path.join(__dirname + '/../public')
-
 var app = express();
+var server = http.createServer(app);
+var IO = socketIO(server);
+
+IO.on('connection', function(socket) {
+    console.log('A user connected');
+    
+    socket.on('createMessage', function(message) {
+        console.log('createMessage', message);
+        IO.emit('BroadcastMessage', {
+            from:message.from,
+            text:message.text,
+            createdAt: new Date().getDate
+        });
+    }); //LISTNER
+
+
+    //Whenever someone disconnects this piece of code executed
+    socket.on('disconnect', function () {
+       console.log('A user disconnected');
+    }); 
+
+    socket.on('recieveEmail', (newEmail) => {
+        console.log('recieveEmail', newEmail) // LISTMER
+    });
+    
+    socket.emit('newMessage', {
+        from: 'akshay',
+        text:'EMIT NEW MESSAGE',
+        createdAt: 12345678901
+    }); // EMITTER USER SENT EMAIL
+
+
+
+    socket.emit('newEmail', {
+        from: 'asdfgh@gmail.com',
+        text:'data from emitter',
+        createdAt: 12345
+    }); // EMITTER USER SENT EMAIL
+
+ 
+});
+ 
+
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log(`STARTING SERVER ON PORT ${port}`);
 });
-
-//console.log(__dirname + '/../public'); // here public and  server are at same level so we use ..                                       // and go one level up
-
+//console.log(__dirname + '/../public'); // here public and  server are at same level so we use and go one level up
 console.log('this is absolute path :', + '\n' + publicPath); 
